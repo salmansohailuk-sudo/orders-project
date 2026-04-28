7d588b82858e7c2932f84f74f46b6d1e183968ef
Salman <salmansohailuk@gmail.com>
2026-04-28 11:41:25 +0100

    hanni 39

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -2,8 +2,19 @@
 
 echo "Zipping lambdas..."
 
-cd lambda/producer && zip -r ../../producer.zip . && cd -
-cd lambda/consumer && zip -r ../../consumer.zip . && cd -
-cd lambda/status && zip -r ../../status.zip . && cd -
+if command -v zip >/dev/null 2>&1; then
+  echo "Using zip..."
+
+  cd lambda/producer && zip -r ../../producer.zip . && cd -
+  cd lambda/consumer && zip -r ../../consumer.zip . && cd -
+  cd lambda/status && zip -r ../../status.zip . && cd -
+
+else
+  echo "zip not found, using PowerShell..."
+
+  powershell Compress-Archive -Path lambda/producer/* -DestinationPath producer.zip -Force
+  powershell Compress-Archive -Path lambda/consumer/* -DestinationPath consumer.zip -Force
+  powershell Compress-Archive -Path lambda/status/* -DestinationPath status.zip -Force
+fi
 
 echo "Done"
